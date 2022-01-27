8f8f29ce392646f65f5d958b9fee7d8baf9a27f5
topiath <topi.raatikainen@gmail.com>
2022-01-27 14:50:26 +0200

    Recreating movement
    Created another player character (no prefab yet) and new movement script for it - works much better now.
    Disabled previous player, but did not delete yet.

---
+++ Team4/Assets/AssetsTeam4/Scripts/CameraChange.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraChange : MonoBehaviour
+{
+    public Camera FirstPersonCam;
+    public Camera ThirdPersonCam;
+
+    void Awake()
+    {
+        FirstPersonCam.enabled = true;
+        ThirdPersonCam.enabled = false;
+    }
+
+
+    void Update()
+    {
+        if (Input.GetButtonDown ("Camera"))
+        {
+            FirstPersonCam.enabled = !FirstPersonCam.enabled;
+            ThirdPersonCam.enabled = !ThirdPersonCam.enabled;
+        }
+        
+       
+    }
+}
--- Team4/Assets/AssetsTeam4/Scripts/MoveCamera.cs
+++ Team4/Assets/AssetsTeam4/Scripts/MoveCamera.cs
@@ -5,9 +5,18 @@ using UnityEngine;
 public class MoveCamera : MonoBehaviour
 {
     public Transform player;
+    public Transform head;
+    public Transform followPoint;
+    public Camera FirstPersonCam;
+    public Camera ThirdPersonCam;
+
+
 
     void Update()
     {
         transform.position = player.transform.position;
+
+        FirstPersonCam.transform.position = head.transform.position;
+        ThirdPersonCam.transform.position = followPoint.transform.position;
     }
 }
